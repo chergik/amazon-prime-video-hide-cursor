01e81eb05de4bcfdd83e220aa34cf87d14d9fb65
Andrey Chergik <achergik@gmail.com>
2020-06-13 23:52:03 -0700

    Fixed the case when cursor sometimes does not diappear.
    Cursor does not disappear when some classes are not applied before the script runs. They apply later and override the 'none' style for the player cursor. Now I use computed cursor style, rather than `element.style.cursor` value. I also added the `primevideo` to domain to the regexp to allow the script to run on that domain. Logging added.

--- amz-prime-video-hide-cursor.js
+++ amz-prime-video-hide-cursor.js
@@ -4,12 +4,15 @@
 // @version      0.1
 // @description  Remove annoying cursor hand while watching the video.
 // @author       Andrey Chergik
-// @include      /^https://www\.amazon\.com/gp/video/.*$/
+// @include      /^https://www\.(amazon|primevideo)\.com/.*$/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    var loggingPrefix = "Amazon-Prime-Video-hide-hand";
+    var logging = false;
 
     var isUndefOrNull = function (o) {
         return (typeof o === 'undefined' || o === null);
@@ -20,7 +23,6 @@
 
     var getDeepNode = function (root, indices) {
         if (isUndefOrNull(root) || isUndefOrNull(indices) || isEmpty(indices)) return null;
-        var childNodes;
         for (var i = 0; i < indices.length; ++i) {
             if (isUndefOrNull(root) || isUndefOrNull(root.childNodes) || indices[i] >= root.childNodes.length) return null;
             root = root.childNodes[indices[i]];
@@ -28,13 +30,30 @@
         return root;
     };
 
+    var log = function(msg) {
+      if (isUndefOrNull(msg)) return;
+      if (!logging) return;
+      console.log(loggingPrefix + ": " + msg);
+    };
+
     var interval = setInterval(function() {
+        log("START");
         var webPlayerUiContainer = document.querySelectorAll(".webPlayerUIContainer")[0];
+      	log("CONT: " + webPlayerUiContainer);
+      	if (isUndefOrNull(webPlayerUiContainer)) return;
+        log("CONT is not null");
         var movieScreen = getDeepNode(webPlayerUiContainer, [0,0,1,0,2]);
+        log("MOVIE SCREEN:" + movieScreen);
         if (isUndefOrNull(movieScreen)) return;
+        log("MOVIE SCREEN.style.cursor: " + movieScreen.style.cursor)
+      	var cursor = window.getComputedStyle(movieScreen).getPropertyValue('cursor');
+        log("CURSOR style: " + cursor);
+        if (cursor !== "pointer") return;
         movieScreen.style.cursor = 'none';
         var controls = movieScreen.childNodes[0];
+        if (isUndefOrNull(controls)) return;
+        log("CONTROLS: " + controls);
         controls.style.cursor = 'auto';
         clearInterval(interval);
-    }, 1000);
+    }, 2000);
 })();
